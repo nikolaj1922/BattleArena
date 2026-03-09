0d60530200b9c234b38dc9aac1ae62a887ed3074
Nik <bna.career@mail.ru>
2026-03-09 19:19:15 +0700

    Рефакторинг RestartPanelModel

--- Assets/_Project/Scripts/UI/RestartPanel/RestartPanelModel.cs
+++ Assets/_Project/Scripts/UI/RestartPanel/RestartPanelModel.cs
@@ -6,10 +6,7 @@ namespace BattleArena.UI.RestartPanel
     public class RestartPanelModel : IDisposable
     {
         private readonly IRestartBattle _battle;
-
         public event Action<string> OnBattleEnded;
-
-        public string WinnerName { get; private set; }
 
         public RestartPanelModel(IRestartBattle battle)
         {
@@ -17,19 +14,16 @@ namespace BattleArena.UI.RestartPanel
             _battle.OnBattleEnded += HandleBattleEnded;
         }
 
-        private void HandleBattleEnded(string winnerName)
-        {
-            WinnerName = winnerName;
-            OnBattleEnded?.Invoke(winnerName);
-        }
-
         public void RestartBattle()
         {
             _battle.RestartBattle();
-            WinnerName = null;
         }
 
         public void Dispose() => _battle.OnBattleEnded -= HandleBattleEnded;
 
+        private void HandleBattleEnded(string winnerName)
+        {
+            OnBattleEnded?.Invoke(winnerName);
+        }
     }
 }
